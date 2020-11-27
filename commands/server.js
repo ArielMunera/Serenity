2fe16b97f5b71bf7ad6f9092f1470bbbf6c214a1
Aurélien MAR <amar@apside.corp.groupe-apside.fr>
2020-11-27 13:38:21 +0100

    Updating server command description

--- commands/server.js
+++ commands/server.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'server',
-    description: 'Server!',
+    description: 'Show some infos about the current server',
     execute(message, args) {
-            message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     },
 };
