20d22d9f276978c4a468d3a76044c3ec7482fe7b
Ariel Munera <42870853+ArielMunera@users.noreply.github.com>
2020-11-27 23:42:14 +0100

    Random gif command and beginning beautiful message commande

---
+++ commands/gif.js
@@ -0,0 +1,40 @@
+const { giphyKey } = require('../config.json');
+const request = require('request');
+module.exports = {
+    name: 'gif',
+    description: 'Call a random gif',
+    usage: "<arguments>",
+    execute(message, args) {
+
+        // Split message to search GIPHY
+        // let splitWord = message.toString().split(" ");
+        let gifWord = "";
+
+        // Loop through incase of multiple word search
+        for (var i = 0; i < args.length; i++) {
+            if (i > 0) {
+                gifWord = gifWord + "+";
+            }
+
+            gifWord = gifWord + args[i];
+        }
+
+        request("http://api.giphy.com/v1/gifs/search?q=" + gifWord + "&api_key=" + giphyKey + "&limit=100", function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Convert body to JSON object
+                let jsonUrl = JSON.parse(body);
+
+                // Get random number to choose GIF
+                let totGif = jsonUrl.data.length;
+
+                if (totGif > 100) {
+                    totGif = 100;
+                }
+
+                let ranNum = Math.floor(Math.random() * totGif);
+
+                message.channel.send(jsonUrl.data[ranNum].url);
+            }
+        });
+    },
+};
