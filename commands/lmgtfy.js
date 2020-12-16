7f302e1bdd2e4d2e730c1f970f76a6e73f302365
ArielMunera <aureliano14@hotmail.fr>
2020-12-16 16:59:12 +0100

    Let Me Google It For You command

---
+++ commands/lmgtfy.js
@@ -0,0 +1,23 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+const prefix = process.env.PREFIX;
+module.exports = {
+    name: 'lmgtfy',
+    description: '',
+    group: 'Fun',
+    args: true,
+    example: `\`${prefix}${this.name} Quelle heure est-il ?\`\n`,
+    execute(message, args) {
+        const data = [];
+        const question = args.join('+');
+        const text = `Voici la solution à [ta question](https://www.lmgtfy.com/?q=${question} \'ta question\'),` +
+            ` c\'est de la part de ${message.author}, il pense que cela pourrait t\'aider !`;
+        data.push(text);
+        const embed = new MessageEmbed()
+            .setColor(0x00bfff)
+            .setDescription(data);
+
+        return message.delete().then(message.channel.send(embed));
+    },
+};
