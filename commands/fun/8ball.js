f76931b20e609162a4ff5eaaab7a3f664a6ddef9
ArielMunera <aureliano14@hotmail.fr>
2021-01-22 13:46:08 +0100

    8ball command

---
+++ commands/fun/8ball.js
@@ -0,0 +1,27 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+const {
+    ball8Answers
+} = require('../../util/8ballAnswers.json');
+const {
+    getRandomNumber
+} = require('../../util/getRandomNumber');
+module.exports = {
+    name: '8ball',
+    aliases: ['8b'],
+    description: 'Répond à votre question',
+    usage: "[question]",
+    guildOnly: true,
+    example: `\n`,
+    group: 'Fun',
+    args: true,
+    execute(message, args) {
+        let answerIndex = getRandomNumber(ball8Answers.length);
+        let answer = ball8Answers[answerIndex - 1];
+        const embed = new MessageEmbed()
+            .setColor(0x00bfff)
+            .setDescription(":8ball:**" + message.author.username + "**, " + answer)
+        message.channel.send(embed);
+    },
+};
